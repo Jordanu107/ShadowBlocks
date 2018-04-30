140b1a38f32966fd8a7bede7c1a75ac4cc04b78c
Jordanu107 <jordanu@student.unimelb.edu.au>
2018-05-01 02:13:37 +1000

    Added countSprites func

--- src/Loader.java
+++ src/Loader.java
@@ -1,3 +1,6 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Loader {	
 	// Converts a world coordinate to a tile coordinate,
 	// and returns if that location is a blocked tile
@@ -12,6 +15,30 @@ public class Loader {
 	 * @return
 	 */
 	public static Sprite[] loadSprites(String filename) {
+		
 		return null;
+	}
+	
+	/**
+	 * Counts the number of sprites in a given file.
+	 * @param filename
+	 * @return
+	 */
+	public static int countSprites(String filename) {
+		int count = 0;
+		try (BufferedReader br =
+			    new BufferedReader(new FileReader(filename))) {
+			String text;
+			// Iterate through file, counting every sprite
+			while ((text = br.readLine()) != null) {
+					String[] split = text.split(",");
+					if (split.length == 3) {
+						count++;
+					}
+			    }
+			} catch (Exception e) {
+			    e.printStackTrace();
+			}
+		return count;
 	}
 }
