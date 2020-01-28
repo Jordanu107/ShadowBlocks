d4c14488812c69b0d38a309fed781d2f769519f0
Jordanu107 <jordanu@student.unimelb.edu.au>
2020-01-29 03:52:47 +1100

    added a function to check if current level is finished i.e. all blocks in targets

--- src/World.java
+++ src/World.java
@@ -1,34 +1,35 @@
+import com.sun.xml.internal.bind.v2.runtime.Coordinator;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 public class World {
 	private static int levelNumber = 0;
-	private static String filename = "res/levels/" + levelNumber + ".lvl";
 	private static ArrayList<Sprite> spriteArray = new ArrayList<>();
+	private ArrayList<Point> targets = new ArrayList<>();
 	private float offsetX;
 	private float offsetY;
 	
 	public World() throws SlickException {
-		spriteArray = Loader.loadSprites(filename);
-		
-		// Offset coordinates to achieve a centered map
-		offsetX = (float) (App.SCREEN_WIDTH / 2.0 - (Loader.xCoord / 2.0) * App.TILE_SIZE);
-		offsetY = (float) (App.SCREEN_HEIGHT / 2.0 - (Loader.yCoord / 2.0) * App.TILE_SIZE);
+		regenerateWorld();
 	}
 	
 	public void update(Input input, int delta) {
+		if (isLevelComplete(getAllSprites(), targets)) {
+			levelNumber++;
+			regenerateWorld();
+		}
 		for (Sprite sprite: spriteArray) {
 			sprite.update(input, delta);
 		}
 
-		if (input.isKeyPressed(input.KEY_N)) {
-			levelNumber++;
-			filename = "res/levels/" + levelNumber + ".lvl";
-			System.out.println(filename);
-//			regenerateWorld(filename);
+		if (input.isKeyPressed(input.KEY_R)) {
+			regenerateWorld();
+		} else if (input.isKeyPressed(input.KEY_Q)) {
+			System.exit(0);
 		}
 	}
 	
@@ -44,11 +45,25 @@ public class World {
 		return spriteArray;
 	}
 
-//	public void regenerateWorld(String filename) {
-//		World.filename = filename;
-//		spriteCount = Loader.countSprites(filename);
-//		spriteArray = new Sprite[spriteCount];
-//		offsetX = (float) (App.SCREEN_WIDTH / 2.0 - (Loader.xCoord / 2.0) * App.TILE_SIZE);
-//		offsetY = (float) (App.SCREEN_HEIGHT / 2.0 - (Loader.yCoord / 2.0) * App.TILE_SIZE);
-//	}
+	/**
+	 * Generates a world with the current level and creates the required sprites
+	 */
+	public void regenerateWorld() {
+		spriteArray = Loader.loadSprites("res/levels/" + levelNumber + ".lvl");
+		targets = Loader.findTargets(getAllSprites());
+		offsetX = (float) (App.SCREEN_WIDTH / 2.0 - (Loader.xCoord / 2.0) * App.TILE_SIZE);
+		offsetY = (float) (App.SCREEN_HEIGHT / 2.0 - (Loader.yCoord / 2.0) * App.TILE_SIZE);
+	}
+
+	public boolean isLevelComplete(ArrayList<Sprite> spriteArray, ArrayList<Point> targets) {
+		for (Sprite sprite : spriteArray) {
+			if (sprite.getType().equals("res/stone.png") || sprite.getType().equals("res/ice.png")) {
+				Point spriteLocation = new Point((int) sprite.getX(), (int) sprite.getY());
+				if (!targets.contains(spriteLocation)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
 }
